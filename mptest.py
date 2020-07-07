f8e5cf291de85f8294a512e44ae41f1c94454007
Alex B <alexandr.belonozhkin@gmail.com>
2020-07-07 18:42:34 +0300

    _

--- mptest.py
+++ mptest.py
@@ -2,9 +2,17 @@
 
 import multiprocessing as mp
 
+def foo(q):
+    q.put('hello')
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
 def main():
     pass
-
-if __name__ == "__main__":
-    main()
 
