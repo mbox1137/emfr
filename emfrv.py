f547269f78efaa668f34522debbbc6eecbe8e136
Alex B <alexandr.belonozhkin@gmail.com>
2020-07-10 20:16:10 +0300

    121

--- emfrv.py
+++ emfrv.py
@@ -14,7 +14,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def dprint(*args, **kwargs):
-    if True:
+    if False:
         print(*args, **kwargs)
 
 #aa=np.array(list(range(60))).reshape((4,5,3))
@@ -34,7 +34,7 @@ def getRectEdges(rect):
                 yield res	#[[xmi,xma],[ymi,ymi],[zmi,zmi]]
     return
 
-if len(sys.argv)!=4:
+if len(sys.argv) not in [3,4]:
     print(f"{sys.argv[0]} data/emfr.dat 0.002 [0.8]")
     sys.exit()
 datfile=sys.argv[1]
@@ -46,7 +46,12 @@ with open(prefix+"/ap.dump", 'rb') as fp:
 with open(prefix+"/field.dump", 'rb') as fp:
     field=pickle.load(fp)
 delta=float(sys.argv[2])
-porog=float(sys.argv[3])
+try:
+    porog=float(sys.argv[3])
+except IndexError:
+    porog=None
+except ValueError:
+    porog=None
 
 """
 ----------------------------------------
@@ -58,24 +63,25 @@ rect: [[1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.1], [-1.0, -1.0, 1.0, -1.0, 1.0, -1.1,
 ----------------------------------------
 """
 def main():
+    global porog
     print('-'*40)
     dprint("field=",field);
 
     print(f"NaNs: {len(np.isnan(field))}")
     q=abs(gp["rect"][1][0]-gp["rect"][0][0])
     field[np.isnan(field)]=np.finfo(float).max
-    fmax=3	#q/(gp["step"][0][0]*20)**2
-    print(f"fmax={fmax}")
-    nh=20
-    bins=np.append(np.linspace(0,fmax,nh), np.finfo(float).max)
-    print(bins)
-    counts, edges = np.histogram(field, bins)
-    print(counts)
-    print(edges)
-    maxind=np.where(counts == np.amax(counts))[0][0]
-    print(maxind)
-    porog=(edges[maxind]+edges[maxind+1])/2
-    print(porog)
+    if not porog:
+        nh=20
+        fmax=5
+        bins=np.append(np.linspace(0,fmax,nh), np.finfo(float).max)
+        dprint(bins)
+        counts, edges = np.histogram(field, bins)
+        dprint(counts)
+        dprint(edges)
+        maxind=np.where(counts == np.amax(counts))[0][0]
+        dprint(maxind)
+        porog=(edges[maxind]+edges[maxind+1])/2
+        dprint(porog)
 
     ixs=np.argwhere(np.abs(field-porog)<delta)
     xs=ap['kip']*ixs+ap['mi']
